c33f82f885a48ce401d612d2f768b6b8e1724a1d
jonathango98 <jonathangoenadibrata@gmail.com>
2022-09-17 22:32:47 -0700

    init

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <JetsonGPIO.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+
+    GPIO::setmode(GPIO::BOARD);
+    GPIO::setup(7, GPIO::OUT);
+
+    int n = 0;
+
+    while (n < 5){
+
+        cout << "ON" << endl;
+        GPIO::output(7, GPIO::HIGH);
+
+        sleep(1);
+
+        cout << "OFF" << endl;
+        GPIO::output(7, GPIO::LOW);
+
+        sleep(1);
+
+        n = n + 1;
+    }
+    
+
+    return 0;
+}
